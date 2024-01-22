d0cd96ab3cfba58c11a6bdcb4ea1c65176ae24c0
willylin207 <>
2024-01-21 16:56:33 -0800

    Make backend deployable as a standalone service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const morgan = require('morgan')
@@ -16,6 +17,7 @@ const logger = morgan((tokens, req, res) => {
     ].join(' ')
 })
 
+app.use(cors())
 app.use(express.json()) // json parser
 app.use(logger)
 
@@ -106,6 +108,6 @@ app.get('/info', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
